b692cf7b26d782d699a0d1c059082dbe2c77f9af
Rich Stone <richard.had@hotmail.de>
2017-05-13 22:28:30 +0200

    add navToggleButton, invisible for now

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -2,65 +2,64 @@ import React, {Component} from 'react'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import Divider from 'material-ui/Divider'
-import FloatingActionButton from 'material-ui/FloatingActionButton'
-import Menu from 'material-ui/svg-icons/navigation/menu'
 import {Link} from 'react-router'
+import {NavToggleButton} from '../styled/NavDrawer'
 
 class NavDrawer extends Component {
-   state = {
-      open: false
-   }
+    state = {
+      open: true,
+      width: 250
+    }
 
-   toggle = () => {
+    toggle = () => {
       this.setState((prevState, props) => {
-         return {
-            open: !prevState.open
-         }
+        return {
+          open: !prevState.open
+        }
       })
-   }
+    }
 
-   render() {
+    render() {
       return (
-         <div>
-            <FloatingActionButton
-               onTouchTap={this.toggle}
+        <div>
+          <NavToggleButton
+            toggle={this.toggle}
+            width={this.state.width}
+          />
+          <Drawer
+            open={this.state.open}
+            width={this.state.width}
+          >
+            <div
+              style={{
+                height: '200px',
+                width: '100%',
+                backgroundColor: 'salmon'
+              }}
             >
-               <Menu/>
-            </FloatingActionButton>
-            <Drawer
-               open={this.state.open}
+              LoginContainer
+            </div>
+            <Divider/>
+            <Link
+              to={'/'}
             >
-               <div style={
-                  {
-                     height: '200px',
-                     width: '100%',
-                     backgroundColor: 'salmon'
-                  }
-               }>
-                  LoginContainer
-               </div>
-               <Divider/>
-               <Link
-                  to={'/'}
-               >
-                  <MenuItem
-                     onTouchTap={this.toggle}
-                     primaryText={'Play'}
-                  />
-               </Link>
-               <Link
-                  to={'/profile '}
-               >
-                  <MenuItem
-                     onTouchTap={this.toggle}
-                     primaryText={'Profile'}
-                  />
-               </Link>
-            </Drawer>
-         </div>
+              <MenuItem
+                onTouchTap={this.toggle}
+                primaryText={'Play'}
+              />
+            </Link>
+            <Link
+              to={'/profile'}
+            >
+              <MenuItem
+                onTouchTap={this.toggle}
+                primaryText={'Profile'}
+              />
+            </Link>
+          </Drawer>
+        </div>
       )
-   }
+    }
 }
-
 
 export default NavDrawer
