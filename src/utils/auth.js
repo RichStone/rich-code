0916fcae8061f65b746a46df1b73fed33445cf4a
Rich Stone <richard.had@hotmail.de>
2017-05-23 18:39:27 +0200

    add authentication class

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,3 +1,74 @@
+import Auth0Lock from 'auth0-lock'
 const authDomain = 'rich.eu.auth0.com'
 const clientID = 'ilAJH2693Z5edqM8LSJ2ZZQXdmeAzXZY'
 
+class AuthService {
+   constructor() {
+      this.lock = new Auth0Lock(clientID, authDomain, {
+         auth: {
+            params: {
+               scope: 'openid email'
+            },
+         },
+      })
+
+      this.showLock = this.showLock.bind(this)
+
+      this.lock.on('authenticatd', this.authProcess.bind(this))
+   }
+
+
+   authProcess = (authResult) => {
+      console.log(authResult)
+   }
+
+   showLock() {
+      this.lock.show()
+   }
+
+   setToken = (authFields) => {
+      let {
+         idToken,
+         exp
+      } = authFields
+
+      localStorage.setItem('idToken', idToken)
+      localStorage.setItem('exp', exp * 1000)
+
+      isCurrent = () => {
+         let expString = localStorage.getItem('exp')
+         if (!expString) {
+            localStorage.removeItem('idToken')
+            return false
+         }
+         let now = new Date()
+         let exp = new Date(parseInt(expString, 10)) //10 is a radix parameter
+         if(exp < now) {
+            this.logout()
+            return false
+         }
+         else {
+            return true
+         }
+      }
+   }
+
+   getToken() {
+      let idToken = localStorage.getItem('idToken')
+      if(this.isCurrent() && idToken) {
+         return idToken
+      }
+      else {
+         localStorage.removeItem('idToken')
+         localStorage.removeItem('exp')
+         return false
+      }
+   }
+
+   logout = () => {
+      localStorage.removeItem('idToken')
+      localStorage.removeItem('exp')
+      location.reload()
+   }
+
+}
